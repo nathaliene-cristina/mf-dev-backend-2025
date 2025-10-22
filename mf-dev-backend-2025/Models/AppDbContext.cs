660c0effa09ed5a02d745a6d95986834a30ded32
Nathaliene <1597478@sga.pucminas.br>
2025-10-22 19:31:12 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-backend-2025/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2025.Models
+{
+    public class AppDbContext : DbContext //a classe AppDbContext vai herdar todas as características e propriedades do DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } //o DbContextOptions vai receber a classe AppDbContext e passa para a base como herança
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
